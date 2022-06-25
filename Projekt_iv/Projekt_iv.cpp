1fdfd3ff7f08708c859fc1dc2ecee48dd8f76065
KZaczkiewicz <zaczkiewiczklaudia@gmail.com>
2022-06-25 23:26:44 +0200

    Porównanie plików

--- Projekt_iv/Projekt_iv.cpp
+++ Projekt_iv/Projekt_iv.cpp
@@ -2,12 +2,41 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if (argc < 2) {
         cout << "Nalezy podac dwie nazwy plikow\n";
         return 0;
     }
+
+    int bitCounter = 0, differentCounter = 0;
+
+    string firstFile = argv[1];
+    string secondFile = argv[2];
+
+    ifstream firstStream(firstFile, ios::binary);
+    ifstream secondStream(secondFile, ios::binary);
+
+    
+
+    char firstChar, secondChar;
+    firstStream >> firstChar;
+    secondStream >> secondChar;
+
+    while (firstChar != EOF && secondChar != EOF) {
+
+    }
+
+    if (firstChar != EOF || secondChar != EOF) {
+        cout << "Pliki powinny byc tej samej dlugosci";
+    }
+    else {
+        double a = differentCounter, b = bitCounter;
+        cout << a / b << "\n";
+    }
+
+
+    firstStream.close();
+    secondStream.close();
 
 
     return 0;
