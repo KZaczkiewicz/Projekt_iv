53ba3b880e4d5a18ce33a6ea5d6ba1a48e7980a4
KZaczkiewicz <zaczkiewiczklaudia@gmail.com>
2022-06-25 22:56:01 +0200

    warunek

---
+++ Projekt_iv/Projekt_iv.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        cout << "Nalezy podac dwie nazwy plikow\n";
+        return 0;
+    }
+
+
+    return 0;
+}
+
